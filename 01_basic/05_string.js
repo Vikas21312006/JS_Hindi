9a6be2cfe1c21694aa311e702684bd861c5cf85b
Vikas Chaurasiya <chaurasiyavikas053@gmail.com>
2026-01-28 13:23:57 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,38 @@
+// const name = "Vikas"
+// const repoCount = 50
+
+// // console.log(name + repoCount + "Value");
+
+// console.log(`hello my name is ${name} and my repo is ${repoCount}`);
+
+const gameName = new String("Vikas-Neelam")
+
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("a"));
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-6, 4)
+console.log(anotherString);
+
+const newStringOne = "      vikas     "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('vikas'))
+
+console.log(gameName.split('-'))
+
+
+
